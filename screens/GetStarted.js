f6bd35e7304a02d75a8c82b8cd69b56f8ae6db7a
Francis Flancia <flanciafrancis@gmail.com>
2024-09-25 01:22:10 +0800

    Your commit message here

--- screens/GetStarted.js
+++ screens/GetStarted.js
@@ -22,7 +22,7 @@ const GetStarted = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 2,
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#FFFFFF', 
